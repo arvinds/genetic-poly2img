5b7388c02da104661c3174da5ad1ba907e478852
Ajay Gopinath <ajgopi124@gmail.com>
2012-08-01 23:46:53 -0700

    refactored file locations and added licenses.

---
+++ src/input-handler.js
@@ -0,0 +1,26 @@
+/**
+ * genetic-poly2img - a genetic programming implementation
+ *
+ * Copyright (c) 2012 Ajay Gopinath
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at your option) any later
+ * version.
+ *
+ * genetic-poly2img is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with genetic-poly2img; if not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author: Ajay Gopinath <ajgopi124(at)gmail(dot)com>
+ */
+
+function setImageURL(url) {
+	if(confirm("This will reset the process. Continue?")) {
+		confirm("Resetting...")
+		changeSourceImage(url);
+	}
+}
